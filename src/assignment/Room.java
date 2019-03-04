1a72d8ef82b2a4c5b4b5ff63b526b7ff6ff5b366
gfuke <greg.fuke@gmail.com>
2019-03-04 14:05:02 +0000

    Create room class

---
+++ src/assignment/Room.java
@@ -0,0 +1,22 @@
+package assignment;
+
+public class Room {
+    int roomNo;
+    int noOfPeople;
+    int price;
+    String type;
+    
+    
+    private int getRoomNo(){
+        return roomNo;
+    }
+    private int getNoOfPeople(){
+        return noOfPeople;
+    }
+    private int getPrice(){
+        return price;
+    }
+    private String getType(){
+        return type;
+    }
+}
