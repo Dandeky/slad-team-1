d1123e2f0981bca368750e855f52dc4af4d226eb
JTRCheetham <w6c71@students.keele.ac.uk>
2019-03-07 16:53:51 +0000

    Homepage

--- src/assignment/Homepage.java
+++ src/assignment/Homepage.java
@@ -9,25 +9,49 @@ import javax.swing.border.TitledBorder;
 
 public class Homepage extends JFrame{
     
-    private final int CANVAS_INITIAL_WIDTH = 800;
-    private final int CANVAS_INITIAL_HEIGHT = 640;
-    private final int CONTROL_PANEL_WIDTH = 640;
-    private final int MESSAGE_AREA_HEIGHT = 100;
+    private final int SCREEN_INITIAL_WIDTH = 1280;
+    private final int SCREEN_INITIAL_HEIGHT = 720;
     
-    private JPanel controlPanel;
+    private final JPanel controlPanel;
+    private final JButton button1, button2, button3, button4, button5, button6;
     
     public Homepage(){
         setTitle("Homepage");
         setLayout(new BorderLayout());
-        setSize(640,640);
         
+        // Button Panel
         controlPanel = new JPanel();
           controlPanel.setBorder(new TitledBorder(new EtchedBorder(), "Homepage"));
-          controlPanel.setPreferredSize(new Dimension(CONTROL_PANEL_WIDTH, CANVAS_INITIAL_HEIGHT));
-          JScrollPane controlPanelScrollPane = new JScrollPane(controlPanel);
-          controlPanelScrollPane.setPreferredSize(new Dimension(CONTROL_PANEL_WIDTH + 30, CANVAS_INITIAL_HEIGHT));
-        add(controlPanelScrollPane, BorderLayout.LINE_START); 
+          controlPanel.setPreferredSize(new Dimension(SCREEN_INITIAL_WIDTH, SCREEN_INITIAL_HEIGHT));
+          //JScrollPane controlPanelScrollPane = new JScrollPane(controlPanel);
+          //controlPanelScrollPane.setPreferredSize(new Dimension(SCREEN_INITIAL_WIDTH, SCREEN_INITIAL_HEIGHT));
+        add(controlPanel, BorderLayout.LINE_START); 
         
+        // Buttons
+          button1 = new JButton("Button 1");
+          button1.setPreferredSize(new Dimension(SCREEN_INITIAL_WIDTH - 200, 50));
+          controlPanel.add(button1);
+        
+          button2 = new JButton("Button 2");
+          button2.setPreferredSize(new Dimension(SCREEN_INITIAL_WIDTH - 200, 50));
+          controlPanel.add(button2);
+          
+          button3 = new JButton("Button 3");
+          button3.setPreferredSize(new Dimension(SCREEN_INITIAL_WIDTH - 200, 50));
+          controlPanel.add(button3);
+          
+          button4 = new JButton("Button 4");
+          button4.setPreferredSize(new Dimension(SCREEN_INITIAL_WIDTH - 200, 50));
+          controlPanel.add(button4);
+          
+          button5 = new JButton("Button 5");
+          button5.setPreferredSize(new Dimension(SCREEN_INITIAL_WIDTH - 200, 50));
+          controlPanel.add(button5);
+          
+          button6 = new JButton("Button 6");
+          button6.setPreferredSize(new Dimension(SCREEN_INITIAL_WIDTH - 200, 50));
+          controlPanel.add(button6);
+          
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         pack();
         setVisible(true);
