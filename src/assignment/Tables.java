5a279e48586836c47542d862dd0df401193915ca
emmanuel994 <obioraxd@icloud.com>
2019-03-11 13:28:01 +0000

    tables class

---
+++ src/assignment/Tables.java
@@ -0,0 +1,15 @@
+package assignment;
+
+public class Tables {
+    private int chairsNo;
+    private int tableNo;
+    
+    
+    private int getChairsNo(){
+        return chairsNo;
+    }
+    private int getTableNo(){
+        return tableNo;
+    }
+    
+}
