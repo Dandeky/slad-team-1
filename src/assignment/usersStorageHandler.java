e28339bc463201e67b26fb2b9023aa6b04512ebc
DJohnson <daniel.johnson@openimagination.co.uk>
2019-03-14 15:31:19 +0000

    remove json libaries

--- src/assignment/usersStorageHandler.java
+++ src/assignment/usersStorageHandler.java
@@ -1,12 +1,12 @@
-package assignment;
-
-import main.java.org.json.simple.*;
-
-public class usersStorageHandler {
-
-    void dfg() {
-        JSONObject thing = new JSONObject();
-    }
-
-
-}
+//package assignment;
+//
+//import main.java.org.json.simple.*;
+//
+//public class usersStorageHandler {
+//
+//    void dfg() {
+//        JSONObject thing = new JSONObject();
+//    }
+//
+//
+//}
