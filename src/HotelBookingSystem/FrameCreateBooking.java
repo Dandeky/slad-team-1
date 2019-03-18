1995ae377bb16aaa3ec44d306b173cdeb775d11e
JTRCheetham <w6c71@students.keele.ac.uk>
2019-03-18 14:09:35 +0000

    CreateBooking

--- src/HotelBookingSystem/FrameCreateBooking.java
+++ src/HotelBookingSystem/FrameCreateBooking.java
@@ -13,6 +13,7 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JSpinner;
 import javax.swing.JTextField;
+import javax.swing.SpinnerDateModel;
 import javax.swing.SpinnerListModel;
 import javax.swing.SpinnerNumberModel;
 import javax.swing.border.EtchedBorder;
@@ -27,6 +28,7 @@ public class FrameCreateBooking extends JFrame {
     private final JButton submitButton;
     private final JCheckBox gymCheck, breakfastCheck, wifiCheck;
     private JTextField nameInput;
+    private final JSpinner room1Spinner, room2Spinner, room3Spinner, room4Spinner, guestSpinner, startDateSpinner, endDateSpinner;
 
     public FrameCreateBooking() {
         setTitle("FrameCreateBooking");
@@ -51,20 +53,60 @@ public class FrameCreateBooking extends JFrame {
         nameInput.setPreferredSize(new Dimension(SCREEN_INITIAL_WIDTH - 50, 50));
         detailPanel.add(nameInput);
         
+        SpinnerNumberModel room1Model = new SpinnerNumberModel(0, 0, 100, 1);
+        JLabel room1Label = new JLabel("Single");
+        detailPanel.add(room1Label);
+        room1Spinner = new JSpinner(room1Model);
+        detailPanel.add(room1Spinner);
+        
+        SpinnerNumberModel room2Model = new SpinnerNumberModel(0, 0, 100, 1);
+        JLabel room2Label = new JLabel("Double");
+        detailPanel.add(room2Label);
+        room2Spinner = new JSpinner(room2Model);
+        detailPanel.add(room2Spinner);
+        
+        SpinnerNumberModel room3Model = new SpinnerNumberModel(0, 0, 100, 1);
+        JLabel room3Label = new JLabel("Family");
+        detailPanel.add(room3Label);
+        room3Spinner = new JSpinner(room3Model);
+        detailPanel.add(room3Spinner);
+        
+        SpinnerNumberModel room4Model = new SpinnerNumberModel(0, 0, 100, 1);
+        JLabel room4Label = new JLabel("Business");
+        detailPanel.add(room4Label);
+        room4Spinner = new JSpinner(room4Model);
+        detailPanel.add(room4Spinner);
+        
+        SpinnerNumberModel guestModel = new SpinnerNumberModel(0, 0, 100, 1);
+        JLabel guestLabel = new JLabel("No. of Total Guests");
+        detailPanel.add(guestLabel);
+        guestSpinner = new JSpinner(guestModel);
+        detailPanel.add(guestSpinner);
+        
         //Duration of Stay Panel
         durationPanel = new JPanel();
         durationPanel.setBorder(new TitledBorder(new EtchedBorder(), "Duration of Stay"));
         durationPanel.setPreferredSize(new Dimension(SCREEN_INITIAL_WIDTH-20, 180));
         controlPanel.add(durationPanel, BorderLayout.LINE_START);
         
-        int currentYear = Date(new Date);
         Calendar calendar = Calendar.getInstance();
         Date initDate = calendar.getTime();
-
-        //String[] monthStrings = getMonthStrings(); //get month names
-        SpinnerNumberModel monthModel = new SpinnerNumberModel(currentYear, currentYear, currentYear + 10, 1);
-        JSpinner spinner = new JSpinner(monthModel);
-        durationPanel.add(spinner);
+        calendar.add(Calendar.DAY_OF_MONTH, -1);
+        Date earliestDate = calendar.getTime();
+        calendar.add(Calendar.YEAR, 200);
+        Date latestDate = calendar.getTime();
+        
+        JLabel startDateLabel = new JLabel("Start Date");
+        durationPanel.add(startDateLabel);
+        SpinnerDateModel startDateModel = new SpinnerDateModel(initDate,earliestDate,latestDate,Calendar.YEAR);
+        startDateSpinner = new JSpinner(startDateModel);
+        durationPanel.add(startDateSpinner);
+        
+        JLabel endDateLabel = new JLabel("End Date");
+        durationPanel.add(endDateLabel);
+        SpinnerDateModel endDateModel = new SpinnerDateModel(initDate,earliestDate,latestDate,Calendar.YEAR);
+        endDateSpinner = new JSpinner(endDateModel);
+        durationPanel.add(endDateSpinner);
         
         //Extras Panel
         extrasPanel = new JPanel();
