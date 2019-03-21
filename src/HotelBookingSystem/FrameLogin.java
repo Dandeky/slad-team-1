ab5e6b209e9fc849549f7b7f53317b7d1ff972cf
gfuke <greg.fuke@gmail.com>
2019-03-21 16:20:33 +0000

    login

--- src/HotelBookingSystem/FrameLogin.java
+++ src/HotelBookingSystem/FrameLogin.java
@@ -2,6 +2,8 @@ package HotelBookingSystem;
 
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.*;
 import javax.swing.border.EtchedBorder;
 import javax.swing.border.TitledBorder;
@@ -15,10 +17,59 @@ class FrameLogin extends JFrame {
     private final JTextField usernameInput, passwordInput;
     private final JButton loginButton;
 
+    
+     class LoginListener implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent event){
+            
+            String username = "";
+            String password = "";
+            try{
+                username = usernameInput.getText();
+                password = passwordInput.getText();
+            }
+            catch(NullPointerException e){
+                System.out.println("At least 1 field is empty");
+            }
+            
+            
+           if (UsersHandler.verifyLogin(username, password)){
+               //Logged in
+               
+       //Test   
+                                                        String[] types = new String[10];
+                                                        
+                                                        for (int i = 0; i < types.length; i++) {
+                                                            if (i % 2 == 0) {
+                                                                types[i] = "single";
+                                                            } else {
+                                                                types[i] = "double";
+                                                            }
+                                                        }
+
+                                                        Hotel testHotel = new Hotel("Test Hotel", types);
+
+               
+              FrameHomepage framehomepage = new FrameHomepage(testHotel, 7);
+             
+              setVisible(false);
+              dispose();
+           }
+           else{
+               System.out.println("Login Failed");
+                }
+        }
+    }//END ActionListener
+      
+    
+    
+    
+    
+    
     FrameLogin() {
 
         controlPanel = new JPanel();
-        controlPanel.setBorder(new TitledBorder(new EtchedBorder(), "FrameLogin"));
+        controlPanel.setBorder(new TitledBorder(new EtchedBorder(), "Login"));
         controlPanel.setPreferredSize(new Dimension(SCREEN_INITIAL_WIDTH, SCREEN_INITIAL_HEIGHT));
         controlPanel.setLayout(new FlowLayout());
         //JScrollPane controlPanelScrollPane = new JScrollPane(controlPanel);
@@ -37,18 +88,17 @@ class FrameLogin extends JFrame {
         passwordInput.setPreferredSize(new Dimension(SCREEN_INITIAL_WIDTH - 50, 50));
         controlPanel.add(passwordInput);
 
-        loginButton = new JButton("FrameLogin");
+        loginButton = new JButton("Login");
         loginButton.setPreferredSize(new Dimension(SCREEN_INITIAL_WIDTH - 200, 50));
         controlPanel.add(loginButton);
+        loginButton.addActionListener(new LoginListener() );
 
-        //TODO: Actionlistener class that sends login data UsersHandler.verifyLogin(username, password)
-        // Pass hotel and user ids to homepage jframe
-
-//        FrameHomepage frameHomepage = new FrameHomepage(hotel, user);
-
-
+       
+    
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         pack();
         setVisible(true);
     }
+
+    
 }
