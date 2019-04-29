fb863b5de7ddc82253a7ff40d52048429e2b98a6
DJohnson <daniel.johnson@openimagination.co.uk>
2019-04-29 11:06:07 +0100

    update

--- src/test/HotelTest.java
+++ src/test/HotelTest.java
@@ -27,33 +27,33 @@ public class HotelTest {
 //            System.out.println(testHotel.getRooms()[i]);
 //        }
 
-//        int[] rooms = new int[4];
-//        rooms[0] = 1;
-//        rooms[1] = 2;
-//        rooms[2] = 3;
-//        rooms[3] = 4;
-//
-//        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
-//        Date dateStart = dateFormat.parse("1/1/2000");
-//        Date dateEnd1 = dateFormat.parse("1/1/2001");
-//        Date dateEnd2 = dateFormat.parse("1/1/2020");
-//
-//        boolean[] booleans = new boolean[3];
-//        booleans[0] = true;
-//        booleans[2] = true;
-//        for (int i = 0; i < 10; i++) {
-//            if (i % 2 == 0) {
-//                testHotel.makeBooking(1, rooms, dateStart, dateEnd1, booleans);
-//            } else {
-//                testHotel.makeBooking(1, rooms, dateStart, dateEnd2, booleans);
-//            }
-//
-//        }
+        int[] rooms = new int[4];
+        rooms[0] = 1;
+        rooms[1] = 2;
+        rooms[2] = 3;
+        rooms[3] = 4;
 
-//        User testUser = new User("test", "testLast", 123123123, "testUsername", "testPassword");
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        Date dateStart = dateFormat.parse("1/1/2000");
+        Date dateEnd1 = dateFormat.parse("1/1/2001");
+        Date dateEnd2 = dateFormat.parse("1/1/2020");
+
+        boolean[] booleans = new boolean[3];
+        booleans[0] = true;
+        booleans[2] = true;
+        for (int i = 0; i < 10; i++) {
+            if (i % 2 == 0) {
+                testHotel.makeBooking(1, rooms, dateStart, dateEnd1, booleans);
+            } else {
+                testHotel.makeBooking(1, rooms, dateStart, dateEnd2, booleans);
+            }
+
+        }
+
+        User testUser = new User("test", "testLast", 123123123, "testUsername", "testPassword");
 
 
-//        FrameHomepage frameHomepage = new FrameHomepage(testHotel, testUser);
+        FrameHomepage frameHomepage = new FrameHomepage(testHotel, testUser);
 
         HotelHandler.saveHotel(testHotel);
         Hotel getHotel = HotelHandler.getHotel();
@@ -66,17 +66,17 @@ public class HotelTest {
 
 //        System.out.println(testHotel.toString());
 
-//        for (int room : testHotel.getAllRooms()) {
-//            System.out.println(room);
-//        }
-//        System.out.println("----------");
-//        for (int roomId : testHotel.getCurrentRoomsBooked()) {
-//            System.out.println(roomId);
-//        }
-//        System.out.println("----------");
-//        for (int typeCount : testHotel.getCurrentAvailRooms()) {
-//            System.out.println(typeCount);
-//        }
+        for (int room : testHotel.getAllRooms()) {
+            System.out.println(room);
+        }
+        System.out.println("----------");
+        for (int roomId : testHotel.getCurrentRoomsBooked()) {
+            System.out.println(roomId);
+        }
+        System.out.println("----------");
+        for (int typeCount : testHotel.getCurrentAvailRooms()) {
+            System.out.println(typeCount);
+        }
 
 //        System.out.println("Booking id 0 is in date? -> " + testHotel.getBookings().get(0).isInDate());
 //        System.out.println(testHotel.getBookings().get(0));
--- src/test/UsersIOTest.java
+++ src/test/UsersIOTest.java
@@ -6,23 +6,21 @@ import HotelBookingSystem.UsersHandler;
 public class UsersIOTest {
     public static void main(String[] args) {
 
-//        for (int i = 0; i < 10; i++) {
-//            UsersHandler.saveUserToFile("Dadanm0n" + i, "password", "0");
-//            UsersHandler.saveUserToFile("Dandeky" + i, "password2", "1");
-//        }
+        for (int i = 0; i < 10; i++) {
+            UsersHandler.saveUserToFile("Dadanm0n" + i, "password", false);
+            UsersHandler.saveUserToFile("Dandeky" + i, "password2", true);
+        }
 
-//        UsersHandler.deleteUser("Dandeky0");
+        UsersHandler.deleteUser("Dandeky0");
 
 //        UsersHandler.deleteAllUsers();
-
-//        System.out.println(UsersHandler.verifyLogin("Dandeky", "password2"));
 
         int[] result = UsersHandler.verifyLogin("Dadanm0n1", "password");
         for (int r : result) {
             System.out.print(r);
         }
 
-//        UsersHandler.deleteUser("Dadanm0n");
+        UsersHandler.deleteUser("Dadanm0n");
 
     }
 }
